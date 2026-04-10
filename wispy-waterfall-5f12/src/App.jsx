e57600d7d037a54361324c85e301a0f5ef867638
Yochanan Harel <yoch21@gmail.com>
2026-04-10 21:11:40 +0300

    links and logo added

--- wispy-waterfall-5f12/src/App.jsx
+++ wispy-waterfall-5f12/src/App.jsx
@@ -1,8 +1,14 @@
+import './App.css'
+
 function App() {
   return (
     <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', textAlign: 'center' }}>
       <img src="/logo.png" alt="חמל דיבה לוגו" style={{ width: '60%' }} />
       <h1>ברוכים הבאים לחמ״ל דיבה</h1>
+      <ul className="links-list">
+        <li><a href="/join">להצטרפות אלינו</a></li>
+        <li><a href="https://beactive.co.il/project/89879">לתרומה למלחמה על האמת</a></li>
+      </ul>
     </div>
   )
 }
